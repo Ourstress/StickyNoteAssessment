7f8ab67f8026f406d72a0e8919700d76c0f2be24
SG <sgng1@e.ntu.edu.sg>
2018-12-10 01:40:08 +0000

    implemented createNote function

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 const todos = document.querySelector("#Todos");
-const helloNote = document.createElement("h1");
-helloNote.textContent = "Hello World Note";
-todos.append(helloNote);
+
+function createNote(titleText, contentText) {
+  const aNote = document.createElement("article");
+  const title = document.createElement("h2");
+  title.textContent = titleText;
+  aNote.append(title);
+  const content = document.createElement("p");
+  content.textContent = contentText;
+  aNote.append(content);
+  todos.append(aNote);
+}
+
+createNote("Hello World", "this is my first post");
+createNote("Yawn", "this is my second post");
