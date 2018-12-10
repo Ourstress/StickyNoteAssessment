0999a6d73e62c0bf48c1ac2bf5766031e2c2f8e4
SG <sgng1@e.ntu.edu.sg>
2018-12-10 01:55:27 +0000

    added localstorage function and minor name change from todos to notes

--- main.js
+++ main.js
@@ -1,15 +1,36 @@
-const todos = document.querySelector("#Todos");
+const notes = document.querySelector("#Notes");
 
-function createNote(titleText, contentText) {
+const notesArray = localStorage.getItem("notes")
+  ? JSON.parse(localStorage.getItem("notes"))
+  : [
+      {
+        noteID: new Date().getTime(),
+        title: "hello world",
+        content: "your content goes here"
+      }
+    ];
+
+function createNote(titleText, contentText, timeNow = new Date().getTime()) {
   const aNote = document.createElement("article");
+  aNote.id = timeNow;
   const title = document.createElement("h2");
   title.textContent = titleText;
   aNote.append(title);
   const content = document.createElement("p");
   content.textContent = contentText;
   aNote.append(content);
-  todos.append(aNote);
+  notes.append(aNote);
 }
 
-createNote("Hello World", "this is my first post");
-createNote("Yawn", "this is my second post");
+// function addNote(){
+//     notesArray.push({ noteID: timeNow, title: titleText, content: contentText });
+//     localStorage.setItem("notes", JSON.stringify(notesArray));
+// }
+
+function onPageLoad() {
+  notesArray.map(function(note) {
+    createNote(note.title, note.content, note.noteID);
+  });
+}
+
+onPageLoad();
