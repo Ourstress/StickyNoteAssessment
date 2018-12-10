648039dcf0d4459dff6299c88a6f835cbb9ccbb4
SG <sgng1@e.ntu.edu.sg>
2018-12-10 03:16:05 +0000

    added editNote function - pending refinement

--- main.js
+++ main.js
@@ -12,13 +12,21 @@ const notesArray = localStorage.getItem("notes")
 
 function createNote(titleText, contentText, timeNow = new Date().getTime()) {
   const aNote = document.createElement("article");
-  aNote.id = timeNow;
+  aNote.id = `id${timeNow}`;
   const title = document.createElement("h2");
   title.textContent = titleText;
   aNote.append(title);
-  const content = document.createElement("p");
+  const content = document.createElement("textarea");
   content.textContent = contentText;
+  content.readOnly = true;
   aNote.append(content);
+  const editButton = document.createElement("button");
+  editButton.textContent = "edit";
+  editButton.addEventListener("click", function(e) {
+    e.preventDefault();
+    editNote(`id${timeNow}`, content);
+  });
+  aNote.append(editButton);
   notes.append(aNote);
 }
 
@@ -27,12 +35,17 @@ function addNote(titleText, contentText) {
   if (titleText !== "" && contentText !== "") {
     createNote(titleText, contentText, timeNow);
     notesArray.push({
-      noteID: timeNow,
+      noteID: `id${timeNow}`,
       title: titleText,
       content: contentText
     });
     localStorage.setItem("notes", JSON.stringify(notesArray));
   }
+}
+
+function editNote(articleKey, content) {
+  const article = document.querySelector(`#${articleKey}`);
+  content.readOnly = false;
 }
 
 function onPageLoad() {
